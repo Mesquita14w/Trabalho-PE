aa7f7261e52eda8438f66096c24549c991a88ee7
Matheus Mesquita <88955151+Mesquita14w@users.noreply.github.com>
2025-07-02 11:38:55 -0300

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -29,27 +29,4 @@ modelo = sm.OLS(y, X).fit()
 # Exibindo o resumo do modelo
 print(modelo.summary())
 
-
-# GRÁFICOS DE RESÍDUOS VS VALORES PREDITOS:
-
-y_pred = modelo.predict(X)
-residuos = y - y_pred
-
-plt.figure(figsize=(10,6))
-sns.scatterplot(x=y_pred, y=residuos)
-plt.axhline(0, color='red', linestyle='--')
-plt.xlabel("Valores preditos")
-plt.ylabel("Resíduos")
-plt.title("Resíduos vs Valores Preditos")
-plt.grid(True)
-plt.show()
-
-
-# HISTOGRAMA DE RESÍDUOS:
-
-plt.figure(figsize=(10,6))
-sns.histplot(residuos, kde=True, bins=15, color='purple')
-plt.xlabel("Resíduos")
-plt.title("Distribuição dos Resíduos")
-plt.grid(True)
-plt.show()
+# OBS: OS CÓDIGOS PARA CONSTRUIR OS GRÁFICOS ESTÃO DISPONÍVEIS NA PASTA GRÁFICOS NESTE MESMO DIRETÓRIO.
